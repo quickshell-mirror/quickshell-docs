2b08a718bdf6d01e3b117f4a40678cfc3d3637b0
outfoxxed <outfoxxed@outfoxxed.me>
2025-06-11 00:07:20 -0700

    nix: update nixpkgs

--- typegen/default.nix
+++ typegen/default.nix
@@ -6,5 +6,5 @@
   version = "0.1.0";
 
   src = nix-gitignore.gitignoreSource [] ./.;
-  cargoSha256 = "4bH7E+NpZPFtr//l00tYkHkRRbI3D0TkKas1M+vDWpI=";
+  cargoHash = "sha256-bOOYzCLIyze6DbtMDupSuRFgJAKjbcFXrZw7vclorYQ=";
 }
